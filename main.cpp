189b096160f0cfe8a614b545b37a7fc020587c87
Gregory Haynes <greg@greghaynes.net>
2012-07-04 23:39:07 -0700

    Move Timer in to seperate source files

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <SDL/SDL_image.h>
 #include <SDL/SDL_ttf.h>
 
+#include "timer.h"
+
 #define WIDTH 800
 #define HEIGHT 600
 #define FRAMES_PER_SECOND 60
@@ -14,77 +16,6 @@ static SDL_Surface *background;
 
 static int ctr_w = WIDTH / 2;
 static int ctr_h = HEIGHT / 2;
-
-class Timer {
-	public:
-		Timer();
-
-		void start();
-		void stop();
-		void pause();
-		void unpause();
-		int get_ticks();
-		bool is_started();
-		bool is_paused(); 
-
-	private:
-		int startTicks;
-		int pausedTicks;
-		bool paused;
-		bool started;
-};
-
-Timer::Timer() {
-	startTicks = 0;
-	pausedTicks = 0;
-	paused = false;
-	started = false;
-}
-
-void Timer::start() { 
-	started = true;
-	paused = false;
-	startTicks = SDL_GetTicks();
-}
-
-void Timer::stop() {
-	started = false;
-	paused = false;
-}
-
-int Timer::get_ticks() {
-	if( started == true ) {
-		if( paused == true ) {
-			return pausedTicks;
-		} else {
-			return SDL_GetTicks() - startTicks;
-		}
-	}
-	return 0;
-}
-
-void Timer::pause() {
-	if( ( started == true ) && ( paused == false ) ) {
-		paused = true;
-		pausedTicks = SDL_GetTicks() - startTicks;
-	}
-}
-
-void Timer::unpause() {
-	if( paused == true ) {
-		paused = false;
-		startTicks = SDL_GetTicks() - pausedTicks;
-		pausedTicks = 0;
-	}
-}
-
-bool Timer::is_started() {
-	return started;
-}
-
-bool Timer::is_paused() {
-	return paused;
-}
 
 static Timer fps;
 static Timer fps_update;
